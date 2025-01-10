7fd527b8feccadc5ff091f6921167a6695dad3cb
Yevhenii Bondarenko <eugenebondarenko76@gmail.com>
2025-01-11 00:53:25 +0200

    fix: prisma deploy

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/schemas/generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
